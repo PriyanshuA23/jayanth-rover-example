97d885aa157b64311a5a18647d79aa04e1e378ee
Priyanshu A <priyanshua@Priyanshus-MacBook-Pro.local>
2026-05-06 00:07:55 +0530

    creates test when rover is dead.

--- test/com/tw/step/rover/commands/CommandExecutionTest.java
+++ test/com/tw/step/rover/commands/CommandExecutionTest.java
@@ -1,6 +1,8 @@
 package com.tw.step.rover.commands;
 
+import com.tw.step.rover.boundary.Boundary;
 import com.tw.step.rover.boundary.InfinitePlateau;
+import com.tw.step.rover.boundary.Plateau;
 import com.tw.step.rover.position.Coordinate;
 import com.tw.step.rover.position.Direction;
 import com.tw.step.rover.position.Navigator;
@@ -24,5 +26,24 @@ class CommandExecutionTest {
 
         new MoveCommand(navigator, boundary).execute(rover);
         assertEquals("0 1 N", rover.toString());
+    }
+
+    @Test
+    void stopExecutingWhenRoverBecomeDead() {
+        Navigator navigator = Navigator.create();
+        Boundary boundary = new Plateau(new Coordinate(0,0), new Coordinate(2,2));
+        Rover rover = new Rover(new Coordinate(0, 0), Direction.N);
+
+        new MoveCommand(navigator, boundary).execute(rover);
+        assertEquals("0 1 N", rover.toString());
+
+        new MoveCommand(navigator, boundary).execute(rover);
+        assertEquals("0 2 N", rover.toString());
+
+        new MoveCommand(navigator, boundary).execute(rover);
+        assertEquals("0 3 N", rover.toString());
+
+        new MoveCommand(navigator, boundary).execute(rover);
+        assertEquals("0 3 N", rover.toString());
     }
 }
